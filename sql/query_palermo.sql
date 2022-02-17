fbac5d380aa40446bee24f9c0d14c76a0e58c2be
Joaq33 <joakoillia@gmail.com>
2022-02-17 01:08:48 -0300

    sentencias en mayus

--- sql/query_palermo.sql
+++ sql/query_palermo.sql
@@ -1,4 +1,4 @@
 SELECT universidad,careers,fecha_de_inscripcion,names,sexo,birth_dates,codigo_postal,direcciones,correos_electronicos
 FROM public.palermo_tres_de_febrero
-where universidad='_universidad_de_palermo'
-and TO_DATE(fecha_de_inscripcion,'DD/Mon/YY') between '01/Sep/20' and '01/Feb/21'
+WHERE universidad='_universidad_de_palermo'
+AND TO_DATE(fecha_de_inscripcion,'DD/Mon/YY') BETWEEN '01/Sep/20' AND '01/Feb/21'
