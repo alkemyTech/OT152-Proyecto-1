6afee2df6265defbe46de6c89c69fa7f682cba77
JoacoAnun <joaco.anun@gmail.com>
2022-02-17 19:13:06 -0300

    Statements alignment

--- sql/query_comahue.sql
+++ sql/query_comahue.sql
@@ -1,11 +1,11 @@
-SELECT universidad,
-	   carrera,
-	   fecha_de_inscripcion,
-	   name,
-	   sexo,
-	   codigo_postal,
-	   direccion,
-	   correo_electronico
+SELECT  universidad,
+        carrera,
+        fecha_de_inscripcion,
+        name,
+        sexo,
+        codigo_postal,
+        direccion,
+        correo_electronico
 FROM flores_comahue
 WHERE universidad = 'UNIV. NACIONAL DEL COMAHUE'
 AND fecha_de_inscripcion BETWEEN '2020-09-1' AND '2021-02-01';
--- sql/query_salvador.sql
+++ sql/query_salvador.sql
@@ -1,11 +1,11 @@
-SELECT universidad,
-       carrera,
-       fecha_de_inscripcion,
-       nombre,
-       sexo,
-       localidad,
-       direccion,
-       email
+SELECT  universidad,
+        carrera,
+        fecha_de_inscripcion,
+        nombre,
+        sexo,
+        localidad,
+        direccion,
+        email
 FROM salvador_villa_maria
 WHERE universidad = 'UNIVERSIDAD_DEL_SALVADOR'
 AND TO_DATE(fecha_de_inscripcion,'DD/Mon/YY') between '01/Sep/20' and '01/Feb/21';
