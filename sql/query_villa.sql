6f059dcc29741e28ccb1a2bf5434dc70ba823b7b
Gian Arias <ariasgian@gmail.com>
2022-02-17 20:26:34 -0300

    se agrega querie de villa maria

--- sql/query_villa.sql
+++ sql/query_villa.sql
@@ -8,5 +8,6 @@ SELECT
   date_part('year',now())-date_part('year',to_date(fecha_nacimiento, 'YYYY-MM-DD')) as age,
   codigo_postal as postal_code,
   correo_electronico as email
+  
 FROM public.flores_comahue
 AND TO_DATE(inscription_dates,'DD-MM-YYYY') BETWEEN '01/09/2020' AND '01/02/2021'
