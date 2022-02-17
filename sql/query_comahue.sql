9a65d0eb1f3ba6569ebb45c323117cea1c256578
Gian Arias <ariasgian@gmail.com>
2022-02-17 20:15:56 -0300

    Calculo de la edad

--- sql/query_comahue.sql
+++ sql/query_comahue.sql
@@ -5,6 +5,7 @@ SELECT
   LEFT(name,STRPOS (name,' ')) as first_name,
   RIGHT(name,STRPOS (name,' ')+1) as last_name,
   sexo as gender,
+  date_part('year',now())-date_part('year',to_date(fecha_nacimiento, 'YYYY-MM-DD')) as age,
   correo_electronico as email
 
 FROM public.flores_comahue
