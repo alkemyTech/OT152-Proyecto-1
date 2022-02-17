e3bf81c4ec2cec4b796f81a42c6169d683e47df1
Joaq33 <joakoillia@gmail.com>
2022-02-17 18:20:33 -0300

    direccion removida

--- sql/query_flores.sql
+++ sql/query_flores.sql
@@ -1,4 +1,4 @@
-SELECT universidad,carrera,fecha_de_inscripcion,name,sexo,fecha_nacimiento,codigo_postal,direccion,correo_electronico 
+SELECT universidad,carrera,fecha_de_inscripcion,name,sexo,fecha_nacimiento,codigo_postal,correo_electronico 
 FROM public.flores_comahue
 WHERE universidad='UNIVERSIDAD DE FLORES'
 AND fecha_de_inscripcion BETWEEN '2020-09-01' AND '2021-02-01'
--- sql/query_palermo.sql
+++ sql/query_palermo.sql
@@ -1,4 +1,4 @@
-SELECT universidad,careers,fecha_de_inscripcion,names,sexo,birth_dates,codigo_postal,direcciones,correos_electronicos
+SELECT universidad,careers,fecha_de_inscripcion,names,sexo,birth_dates,codigo_postal,correos_electronicos
 FROM public.palermo_tres_de_febrero
 WHERE universidad='_universidad_de_palermo'
 AND TO_DATE(fecha_de_inscripcion,'DD/Mon/YY') BETWEEN '01/Sep/20' AND '01/Feb/21'
