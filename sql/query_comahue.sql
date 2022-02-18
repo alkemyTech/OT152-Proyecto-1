e3e07449370c428036928e70d75b8e0bb94a9c55
JoacoAnun <joaco.anun@gmail.com>
2022-02-18 16:03:32 -0300

    Change column reference names, added birth_dates in both queries

--- sql/query_comahue.sql
+++ sql/query_comahue.sql
@@ -1,11 +1,11 @@
-SELECT  universidad,
-        carrera,
-        fecha_de_inscripcion,
+SELECT  universidad AS university,
+        carrera AS career,
+        fecha_de_inscripcion AS inscription_date,
         name,
-        sexo,
-        codigo_postal,
-        direccion,
-        correo_electronico
-FROM flores_comahue
+        sexo AS sex,
+        fecha_nacimiento AS birth_date,
+        codigo_postal AS zip_code,
+        correo_electronico AS email
+FROM public.flores_comahue
 WHERE universidad = 'UNIV. NACIONAL DEL COMAHUE'
 AND fecha_de_inscripcion BETWEEN '2020-09-1' AND '2021-02-01';
--- sql/query_salvador.sql
+++ sql/query_salvador.sql
@@ -1,11 +1,11 @@
-SELECT  universidad,
-        carrera,
-        fecha_de_inscripcion,
-        nombre,
-        sexo,
-        localidad,
-        direccion,
+SELECT  universidad AS university,
+        carrera AS career,
+        fecha_de_inscripcion AS inscription_date,
+        nombre AS name,
+        sexo AS sex,
+        fecha_nacimiento AS birth_date,
+        localidad AS location,
         email
-FROM salvador_villa_maria
+FROM public.salvador_villa_maria
 WHERE universidad = 'UNIVERSIDAD_DEL_SALVADOR'
 AND TO_DATE(fecha_de_inscripcion,'DD/Mon/YY') between '01/Sep/20' and '01/Feb/21';
