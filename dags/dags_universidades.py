16d9796ef03bfe07c5ed7a83a12b9485b21cf75d
Gian Arias <ariasgian@gmail.com>
2022-02-17 21:54:27 -0300

    configuracion tareas segun universidades

--- dags/dags_universidades.py
+++ dags/dags_universidades.py
@@ -4,12 +4,10 @@ from airflow.operators.dummy import DummyOperator
 
 with DAG(
     'universidades',
-    description= 'este es un tutorial de dags',
+    description= 'ETL para dos universidades',
     schedule_interval= timedelta(hours=1),
-    start_date=datetime(2022, 1, 17),
+    start_date=datetime(2022, 2, 17),
 ) as dag:
-    tarea_1= DummyOperator(task_id='tarea_1')
-    tarea_2= DummyOperator(task_id='tarea_2')
-    tarea_3= DummyOperator(task_id='tarea_3')
-
-    tarea_1>>[tarea_2,tarea_3]
+    tarea_1= DummyOperator(task_id='universidad de ciencias sociales')
+    tarea_2= DummyOperator(task_id='universidad J.F. Kenedy')
+    [tarea_1,tarea_2]
