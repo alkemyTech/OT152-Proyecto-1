e5e13eaa4960fd26e9875865cc12665b2ce02443
JoacoAnun <joaco.anun@gmail.com>
2022-02-18 15:53:42 -0300

    Added missing tasks, variable names more clear

--- dags/Universidades_A.py
+++ dags/Universidades_A.py
@@ -1,6 +1,8 @@
+from datetime import datetime, timedelta
+
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
-from datetime import datetime, timedelta
+
 
 with DAG(
     'Universities_A_dags',
@@ -11,9 +13,17 @@ with DAG(
     start_date=(datetime(2022, 2, 18))
 ) as dag:
 
-    # Tasks
-    task_1 = DummyOperator(task_id='ELT_Universidad_De_Flores')
-    task_2 = DummyOperator(task_id='ELT_Universidad_Nacional_De_Villa_Maria')
+    # Tasks Univerdisdad las Flores
+    sql_universidad_flores = DummyOperator(task_id='sql_universidad_flores')
+    processing_universidad_flores = DummyOperator(task_id='processing_universidad_flores')
+    load_universidad_flores = DummyOperator(task_id='load_universidad_flores')
+
+    # Tasks Universidad Nacional de Villa Maria
+    sql_universidad_villa_maria = DummyOperator(task_id='ELT_Universidad_Nacional_De_Villa_Maria')
+    processing_universidad_villa_maria = DummyOperator(task_id='processing_universidad_villa_maria')
+    load_universidad_villa_maria = DummyOperator(task_id='load_universidad_villa_maria')
 
     # Tasks order
-    task_1 >> task_2
+    sql_universidad_flores >> processing_universidad_flores >> load_universidad_flores
+
+    sql_universidad_villa_maria >> processing_universidad_villa_maria >> load_universidad_villa_maria
