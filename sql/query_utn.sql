1bec82cf586494d756379655e33ffa1b5e58ecd6
Joel Solaligue <solaligue.joel98@gmail.com>
2022-02-18 17:19:38 -0300

    Eliinando archivos

--- sql/query_utn.sql
+++
@@ -1,5 +0,0 @@
-SELECT university,career,inscription_date,nombre,birth_date,location,email 
-FROM jujuy_utn 
-WHERE university='universidad tecnológica nacional' 
-AND inscription_date BETWEEN '2020/01/09' AND '2021/02/01'
-
