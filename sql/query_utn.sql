5f175f6f05f7a85bdda2171b30e064b23d41713d
Joel Solaligue <solaligue.joel98@gmail.com>
2022-02-18 10:52:25 -0300

    Agregando query utn

--- sql/query_utn.sql
+++ sql/query_utn.sql
@@ -1,5 +1,5 @@
 SELECT university,career,inscription_date,nombre,birth_date,location,email 
 FROM jujuy_utn 
 WHERE university='universidad tecnológica nacional' 
-AND inscription_date BETWEEN '2020/01/09' AND '2021/02/01
+AND inscription_date BETWEEN '2020/01/09' AND '2021/02/01'
 
