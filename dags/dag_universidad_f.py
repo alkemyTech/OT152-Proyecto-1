40661dcb18fffb4317724762ccdea2ec0af79d8f
Tomas <julliervh@gmail.com>
2022-02-18 19:20:38 -0300

    changed files name

---
+++ dags/dag_universidad_f.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from datetime import timedelta, datetime
+from airflow.operators.dummy import DummyOperator
+
+
+with DAG(
+    'Query_Universidad_F',
+    description='Realizar consultas sobre Universidad de Moron y Rio Cuarto',
+    schedule_interval=timedelta(hours=1), #ejecución cada una hora
+    start_date=datetime(2022, 2, 19) #Puse esa fecha porque no fue aclarada en la consigna
+
+) as dag:
+    #Queries usando la función read_sql de pandas
+    tarea1 = DummyOperator(task_id='Query_F1') #Universidad de Morón
+    tarea2 = DummyOperator(task_id='Query_F2') #Universidad de Río Cuarto
+    
+    tarea1 >> tarea2
