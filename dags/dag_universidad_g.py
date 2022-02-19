529e614d626aa5c1e6ccc988da4a64c1788c5dee
Gian Arias <ariasgian@gmail.com>
2022-02-19 16:03:32 -0300

    crear conexion postgresql

--- dags/dag_universidad_g.py
+++ dags/dag_universidad_g.py
@@ -2,8 +2,30 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
 import pandas as pd
+from airflow.settings import Session
+
+def create_new_conn(session, attributes):
+        new_conn = models.Connection()
+        new_conn.conn_id = attributes.get("conn_id")
+        new_conn.conn_type = attributes.get('conn_type')
+        new_conn.host = attributes.get('host')
+        new_conn.port = attributes.get('port')
+        new_conn.schema = attributes.get('schema')
+        new_conn.login = attributes.get('login')
+        new_conn.set_password(attributes.get('password'))
+
+        session.add(new_conn)
+        session.commit()
 
 
+create_new_conn(session,
+                    {"conn_id": "postgres_1",
+                     "conn_type": "postgres",
+                     "host": "training-main.cghe7e6sfljt.us-east-1.rds.amazonaws.com",
+                     "port": 5432,
+                     "schema": "public",
+                     "login": "alkymer",
+                     "password": "alkymer_123"})
 
 with DAG(
     'universidades',
