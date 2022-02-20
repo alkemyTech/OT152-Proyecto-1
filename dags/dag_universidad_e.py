fc239f829a66c3498c8b70bb8fd7efbf6626d670
Tadeo Vardánega <tadeo.vardanega@gmail.com>
2022-02-20 12:52:43 -0300

    Renombre los nombres de las variables

--- dags/dag_universidad_e.py
+++ dags/dag_universidad_e.py
@@ -10,10 +10,10 @@ with DAG(
     schedule_interval=timedelta(hours=1),   
     start_date=datetime(2022, 2, 18)
 ) as dag:
-    query_moron = DummyOperator(task_id='query_moron')  #Voy a usar un  PostgresOperator para ejecutar la querie
-    query_riocuarto = DummyOperator(task_id='query_riocuarto') #Voy a usar un  PostgresOperator para ejecutar la querie
+    query_pampa = DummyOperator(task_id='query_pampa')  #Voy a usar un  PostgresOperator para ejecutar la querie
+    query_interamericana = DummyOperator(task_id='query_interamericana') #Voy a usar un  PostgresOperator para ejecutar la querie
     procesamiento_datos = DummyOperator(task_id='procesamiento_datos') #Voy a usar un PythonOperator para procesar los datos
     subir_s3 = DummyOperator(task_id='subir_s3') #Voy a usar un S3 operator para subir los datos a S3
 
-    [query_moron, query_riocuarto] >> procesamiento_datos >> subir_s3
+    [query_pampa, query_interamericana] >> procesamiento_datos >> subir_s3
 
