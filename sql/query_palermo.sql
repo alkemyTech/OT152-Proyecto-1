583ef4d230b51841f70509fac608d3156704fe9b
Joaq33 <joakoillia@gmail.com>
2022-02-21 15:15:46 -0300

    fix format, univ jujuy and column names

--- sql/query_palermo.sql
+++ sql/query_palermo.sql
@@ -1,4 +1,15 @@
-SELECT universidad,careers,fecha_de_inscripcion,names,sexo,birth_dates,codigo_postal,correos_electronicos
-FROM public.palermo_tres_de_febrero
-WHERE universidad='_universidad_de_palermo'
-AND TO_DATE(fecha_de_inscripcion,'DD/Mon/YY') BETWEEN '01/Sep/20' AND '01/Feb/21'
+SELECT
+    universidad,
+    careers AS carrera,
+    fecha_de_inscripcion,
+    names AS nombre,
+    sexo,
+    birth_dates AS fecha_de_nacimiento,
+    codigo_postal,
+    correos_electronicos AS email
+FROM
+    public.palermo_tres_de_febrero
+WHERE
+    universidad = '_universidad_de_palermo'
+    AND TO_DATE(fecha_de_inscripcion, 'DD/Mon/YY') BETWEEN '01/Sep/20'
+    AND '01/Feb/21'
