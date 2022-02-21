d14c0e5236e2e448002c8b95f58c677cdd649ec9
Tadeo Vardánega <tadeo.vardanega@gmail.com>
2022-02-20 21:34:27 -0300

    Arregle la consulta de Rio Cuarto

--- sql/query_rio_cuarto.sql
+++ sql/query_rio_cuarto.sql
@@ -1,4 +1,6 @@
 SELECT univiersities, carrera, inscription_dates, 
 names, sexo, localidad, email 
 FROM rio_cuarto_interamericana
-WHERE inscription_dates BETWEEN '20/Sep/01' AND '21/Feb/01';
+WHERE univiersities = 'Universidad-nacional-de-rÝo-cuarto' 
+AND inscription_dates BETWEEN '20/Sep/01' AND '21/Feb/01';
+
