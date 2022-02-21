311bb030a4220b7cb8e9797eca4e5f96577af0c2
Joel Solaligue <solaligue.joel98@gmail.com>
2022-02-21 12:06:52 -0300

    standardizing query columns

--- sql/query_tres_de_febrero.sql
+++ sql/query_tres_de_febrero.sql
@@ -1,4 +1,5 @@
-SELECT universidad, careers,fecha_de_inscripcion,names,sexo,birth_dates,codigo_postal,correos_electronicos 
+SELECT  universidad AS university , careers AS career, fecha_de_inscripcion AS inscription_date,
+        names ,sexo AS gender,birth_dates,codigo_postal AS postal_code,correos_electronicos AS email
 FROM palermo_tres_de_febrero 
 WHERE universidad='universidad_nacional_de_tres_de_febrero' 
 AND TO_DATE(fecha_de_inscripcion,'DD/Mon/YY') BETWEEN '01/Sep/20' AND '01/Feb/21'
