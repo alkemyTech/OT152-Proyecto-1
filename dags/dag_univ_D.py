124a3d484a217b64e7a06c5acb6059a7a004bd53
penalozale <penaloza.le@gmail.com>
2022-02-21 09:35:00 -0300

    PR Fixes

--- dags/dag_univ_D.py
+++ dags/dag_univ_D.py
@@ -2,36 +2,32 @@ from datetime import timedelta, datetime
 from airflow import DAG   
 from airflow.operators.dummy import DummyOperator
 
-default_args = {
-	description = "ETL para dos universidades",
-	schedule_interval = "@hourly"
-	start_date = datetime(2022,2,18)
-}
-
-with DAG(dag_id="universidades_D", default_args=default_args) as dag:
-	"""
+with DAG(
+    dag_id = "ETL para dos universidades", 
+    description : "ETL para dos universidades",
+    scheduyle_interval : "@hourly",
+    start_date : datetime(2002,2,18)
+    ) as dags:
+    """
     Se harán dos consultas SQL para dos universidades: UTN y Tres de Febrero
-    Usaremos los siguientes operadores: 
-		airflow.providers.postgres.operators.postgres -> PostgresOperator
-
+    usaremos los siguientes operadores:
+        airflow.providers.postgres.operators.postgres -> PostgresOperator
+    
     Procesamiento de los datos:
         airflow.operators.python_operator -> PythonOperator
-        import pandas as pd
-
+    
     Carga de datos a S3:
         airflow.providers.amazon.aws.operators.s3
-	"""
-	
-    sql_query_utn = DummyOperator(task_id="univ. tecnologica")
-	sql_query_tres_de_febrero = DummyOperator(task_id="univ tres de febrero")
+    """
+    sql_query_utn = DummyOperator(task_id = "sql_query_utn")
+    sql_query_tres_de_febrero = DummyOperator(task_id = "sql_query_tres_de_febrero")
 
-    proc_utn = DummyOperator(task_id="proc utn")
-    proc_tres_de_febrero = DummyOperator(task_id="proc tres de febrero")
+    procesar_utn = DummyOperator(task_id="procesar_utn")
+    procesar_tres_de_febrero = DummyOperator(task_id="procesar_tres_de_febrero")
 
-    uploadS3_utn = DummyOperator(task_id="Carga S3 utn")
-    uploadS3_tres_de_febrero = DummyOperator(task_id="Carga S3 tres de febrero")
+    uploads3_utn = DummyOperator(task_id="uploads3_utn")
+    uploads3_tres_de_febrero = DummyOperator(task_id="uploads3_tres_de_febrero")
+    
+    sql_query_utn >> procesar_utn >> uploads3_utn
+    sql_query_tres_de_febrero >> procesar_tres_de_febrero >> uploads3_tres_de_febrero
 
-
-	
-sql_query_utn >> proc_utn >> uploadS3_utn
-sql_query_tres_de_febrero >> proc_tres_de_febrero >> uploadS3_tres_de_febrero
