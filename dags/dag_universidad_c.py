3908779cd0b52bb621f4d1232bf63352e50782eb
Leono <penaloza.le@gmail.com>
2022-02-22 00:49:16 -0300

    Add retries and retry_delay (#23)
    Co-authored-by: julianpalmerio <julianpalmerio@hotmail.com>

--- dags/dag_universidad_c.py
+++ dags/dag_universidad_c.py
@@ -1,10 +1,17 @@
 from datetime import timedelta,datetime
 from airflow import DAG 
 from airflow.operators.dummy import DummyOperator
- 
+
+
+default_args = {
+    'retries' : 5,
+    'retry_delay' : timedelta(minutes=5)
+}
+
 with DAG(
     'universidades_c',
     description='university_processes',
+    default_args= default_args,
     #defino ejecucion
     schedule_interval=timedelta(hours=1),
     start_date=datetime(2022,2,18)
@@ -22,5 +29,6 @@ with DAG(
     
     universidad_nacional= DummyOperator(task_id='universidad_nacional')
     universidad_de_palermo= DummyOperator(task_id='universidad_de_Palermo')
+
 
     universidad_nacional >> universidad_de_palermo
