d7ca580e1d982ac08365bacbbd49d9aa91ca9d14
SolaligueJoel <81047856+SolaligueJoel@users.noreply.github.com>
2022-02-22 00:33:01 -0300

    Configurando DAG (#8)
    * Agregando queries
    
    * Agregando query utn
    
    * Configuracion DAG
    
    * Modificando configuraciones
    
    * Eliinando archivos
    
    * correcting errors
    
    * modifying name dag.py
    
    * deleting wrong file
    
    * Modifying comments from Spanish to English

---
+++ dags/dag_universidad_c.py
@@ -0,0 +1,26 @@
+from datetime import timedelta,datetime
+from airflow import DAG 
+from airflow.operators.dummy import DummyOperator
+ 
+with DAG(
+    'universidades_c',
+    description='university_processes',
+    #defino ejecucion
+    schedule_interval=timedelta(hours=1),
+    start_date=datetime(2022,2,18)
+    ) as dag:
+    
+    
+    #Queries
+    #SQL for Universidad Nacional
+    #SQL for Universidad de Palermo
+    
+    #Data processing 
+    #pandas as pd 
+    
+    #Upload data to S3    
+    
+    universidad_nacional= DummyOperator(task_id='universidad_nacional')
+    universidad_de_palermo= DummyOperator(task_id='universidad_de_Palermo')
+
+    universidad_nacional >> universidad_de_palermo
