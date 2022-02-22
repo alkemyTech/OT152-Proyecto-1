7d04a8d3e0c7e32011235b8a07ac7a9ec00578c2
Tomas <julliervh@gmail.com>
2022-02-22 13:41:36 -0300

    fixing a mistake

--- dags/dag_universidad_d.py
+++ dags/dag_universidad_d.py
@@ -5,4 +5,4 @@ from time import strftime
 logging.basicConfig(level=logging.INFO, datefmt=strftime("%Y-%m-%d"),
                     format='%(asctime)s - %(name)s - %(message)s')
 
-logging.getLogger('Universidad_d')
+logger = logging.getLogger('Universidad_d')
