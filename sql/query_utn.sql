f50e63ab3a2c4b6ea1aea77d6456f08f6659aecc
julianpalmerio <julianpalmerio@hotmail.com>
2022-02-21 21:45:16 -0300

    remove junk characters

--- sql/query_utn.sql
+++ sql/query_utn.sql
@@ -1,5 +1,4 @@
 SELECT university,career,inscription_date,nombre AS names,sexo AS gender,birth_date,location,email 
 FROM public.jujuy_utn 
 WHERE university='universidad tecnológica nacional' 
-AND inscrip-ñtion_date BETWEEN '2020/01/09' AND '2021/02/01'
-
+AND inscription_date BETWEEN '2020/01/09' AND '2021/02/01'
