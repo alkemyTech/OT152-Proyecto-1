d81de41f5f7f649e3c57961cb338ed5deb3498c4
Joel Solaligue <solaligue.joel98@gmail.com>
2022-02-22 14:47:46 -0300

    fixing dag tasks

--- dags/dag_universidad_c.py
+++ dags/dag_universidad_c.py
@@ -8,19 +8,20 @@ with DAG(
     #defino ejecucion
     schedule_interval=timedelta(hours=1),
     start_date=datetime(2022,2,18)
-    ) as dag:
-    
-    
-    #Queries
-    #SQL for Universidad Nacional
-    #SQL for Universidad de Palermo
-    
-    #Data processing 
-    #pandas as pd 
-    
-    #Upload data to S3    
-    
-    universidad_nacional= DummyOperator(task_id='universidad_nacional')
-    universidad_de_palermo= DummyOperator(task_id='universidad_de_Palermo')
+    )as dag:    
+        #Queries
+        #SQL for Universidad Nacional
+        #SQL for Universidad de Palermo
+        
+        #Data processing 
+        #pandas as pd 
+        
+        #Upload data to S3    
+        
+        universidad_nacional = DummyOperator(task_id='universidad_nacional')
+        universidad_de_palermo = DummyOperator(task_id='universidad_de_palermo')
+        query_sql = DummyOperator(task_id='query_sql')
+        data_processing = DummyOperator(task_id='data_processing')
+        upload_s3 = DummyOperator(task_id='uploads3')
 
-    universidad_nacional >> universidad_de_palermo
+        [universidad_nacional,universidad_de_palermo] >> query_sql >> data_processing >> upload_s3
