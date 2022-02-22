1287bc16f2103020fdf8df906013356f4d7850b7
Gian Arias <ariasgian@gmail.com>
2022-02-22 18:22:16 -0300

    correction of date Format 'DD-Mon-YY'

--- sql/query_villa.sql
+++ sql/query_villa.sql
@@ -1,13 +1,13 @@
 SELECT 
   universidad as university,
   carrera as career,
-  fecha_de_inscripcion as incription_date,
-  LEFT(name,STRPOS (name,' ')) as first_name,
-  RIGHT(name,STRPOS (name,' ')+1) as last_name,
+  fecha_de_inscripcion as inscription_date,
+  LEFT(nombre,STRPOS (nombre,'_')) as first_name,
+  RIGHT(nombre,STRPOS (nombre,'_')+1) as last_name,
   sexo as gender,
-  date_part('year',now())-date_part('year',to_date(fecha_nacimiento, 'YYYY-MM-DD')) as age,
-  codigo_postal as postal_code,
-  correo_electronico as email
+  date_part('year',now())-date_part('year',to_date(fecha_nacimiento, 'DD-Mon-YY')) as age,
+  email
   
-FROM public.flores_comahue
-AND TO_DATE(inscription_dates,'DD-MM-YYYY') BETWEEN '01/09/2020' AND '01/02/2021';
+FROM public.salvador_villa_maria
+WHERE universidad= 'UNIVERSIDAD_NACIONAL_DE_VILLA_MARÍA'
+AND TO_DATE(fecha_de_inscripcion,'DD-Mon-YY') BETWEEN '01/09/2020' AND '01/02/2021'
