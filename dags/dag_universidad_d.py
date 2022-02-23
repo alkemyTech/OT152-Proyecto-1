420978319b223a012458e03445e4de0055c83783
Tomas Jullier <90071480+vzta@users.noreply.github.com>
2022-02-23 00:36:28 -0300

    Ot152 243 loggings d (#29)
    * Doing the changes needed for the task
    
    * fixing a mistake
    
    Co-authored-by: julianpalmerio <julianpalmerio@hotmail.com>

--- dags/dag_universidad_d.py
+++ dags/dag_universidad_d.py
@@ -1,6 +1,11 @@
-from datetime import timedelta, datetime
-from airflow import DAG   
+import logging
+from datetime import timedelta
+from time import strftime
 
+logging.basicConfig(level=logging.INFO, datefmt=strftime("%Y-%m-%d"),
+                    format='%(asctime)s - %(name)s - %(message)s')
+
+logger = logging.getLogger('Universidad_d')
 
 #configuro los retries acorde a lo que pide la tarea
 default_args = {
