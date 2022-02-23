86570d74abede29dbc1d1a50529e4416cc9408c4
Tadeo Vardánega <tadeo.vardanega@gmail.com>
2022-02-23 09:48:17 -0300

    Añadi las fechas de nacimiento

--- sql/query_rio_cuarto.sql
+++ sql/query_rio_cuarto.sql
@@ -1,6 +1,6 @@
 SELECT univiersities, carrera, inscription_dates, 
-names, sexo, localidad, email 
+names, sexo, localidad, email, fechas_nacimiento
 FROM rio_cuarto_interamericana
-WHERE univiersities = 'Universidad-nacional-de-rÝo-cuarto' 
+WHERE univiersities = 'Universidad-nacional-de-río-cuarto' 
 AND inscription_dates BETWEEN '20/Sep/01' AND '21/Feb/01';
 
