57d52adaefc211106ee243a82b5010a81b6135ec
ariasgian <51102814+ariasgian@users.noreply.github.com>
2022-02-24 13:27:19 -0300

    Ot152 230 dag g (#18)
    * commit inicial
    
    * agregado de tiempo 1 hora
    
    * configuracion tareas segun universidades
    
    * se coloca operador Postgresql
    
    * configurada tabla de universidad
    
    * cambio de nombre
    
    * borrado de archivo nombre anterior
    
    * script functional
    
    * setup queries by file name .sql
    
    * crear conexion postgresql
    
    * setup name conextion= postgres_default
    
    * correction session and model import
    
    * name correction universidad_kenedy
    
    * build conection PostgesOperator comment
    
    * clean code
    
    * update university name
    
    * delete comma
    
    * cut description and add comma
    
    * rename DAG
    
    * update description

--- dags/dag_universidad_g.py
+++ dags/dag_universidad_g.py
@@ -1,6 +1,14 @@
+
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
 import logging
 from datetime import timedelta
 
+default_args = {
+    "retries": 5,  # Try 5 times
+    "retry_delay": timedelta(minutes=5),  # Wait 5 minutes between retries
+}
 
 # Basic configuration of the format and instantiation of the logger
 logging.basicConfig(
@@ -9,7 +17,13 @@ logging.basicConfig(
         datefmt='%Y-%m-%d')
 logger = logging.getLogger('univ_g')
 
-default_args = {
-    "retries": 5,  # Try 5 times
-    "retry_delay": timedelta(minutes=5),  # Wait 5 minutes between retries
-}
+with DAG(
+    'dag_universidad_g',
+    description= 'ETL univ Facultad Latinoamericana De Ciencias Sociales y Universidad J. F. Kennedy',
+    schedule_interval= "@hourly", #funciona cada hora
+    start_date=datetime(2022, 2, 17)
+) as dag:
+
+    tarea_1= DummyOperator(task_id='universidad_sociales') 
+    tarea_2= DummyOperator(task_id='universidad_kenedy')
+    [tarea_1,tarea_2]
