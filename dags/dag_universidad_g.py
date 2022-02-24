7996ffe4a08fe2a4c0dfa37fb632fe81dda0b040
Gian Arias <ariasgian@gmail.com>
2022-02-23 21:45:20 -0300

    delete comma

--- dags/dag_universidad_g.py
+++ dags/dag_universidad_g.py
@@ -7,7 +7,7 @@ with DAG(
     'universidades',
     description= 'ETL para dos univ Facultad Latinoamericana De Ciencias Sociales yUniversidad J. F. Kennedy'
     schedule_interval= "@hourly", #funciona cada hora
-    start_date=datetime(2022, 2, 17),
+    start_date=datetime(2022, 2, 17)
 ) as dag:
 
     tarea_1= DummyOperator(task_id='universidad_sociales') 
