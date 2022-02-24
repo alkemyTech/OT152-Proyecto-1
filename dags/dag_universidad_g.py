da33f831220cbf2a20be4a475528c9bc2d58b1cd
Gian Arias <ariasgian@gmail.com>
2022-02-23 21:35:11 -0300

    clean code

--- dags/dag_universidad_g.py
+++ dags/dag_universidad_g.py
@@ -1,34 +1,9 @@
 from datetime import datetime
 from airflow import DAG, models
-import airflow
 from airflow.operators.dummy import DummyOperator
-import pandas as pd
-from airflow.settings import Session
-from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 
-def create_new_conn(session, attributes):
-        new_conn = models.Connection()
-        new_conn.conn_id = attributes.get("conn_id")
-        new_conn.conn_type = attributes.get('conn_type')
-        new_conn.host = attributes.get('host')
-        new_conn.port = attributes.get('port')
-        new_conn.schema = attributes.get('schema')
-        new_conn.login = attributes.get('login')
-        new_conn.set_password(attributes.get('password'))
 
-        session.add(new_conn)
-        session.commit()
-
-
-# create_new_conn(Session,
-#                     {"conn_id": "postgres_default",
-#                      "conn_type": "postgres",
-#                      "host": "training-main.cghe7e6sfljt.us-east-1.rds.amazonaws.com",
-#                      "port": 5432,
-#                      "schema": "public",
-#                      "login": "alkymer",
-#                      "password": "alkymer_123"})
 
 with DAG(
     'universidades',
@@ -36,21 +11,6 @@ with DAG(
     schedule_interval= "@hourly",
     start_date=datetime(2022, 2, 17),
 ) as dag:
-
-    # usar PostgresOperator ejecutar query
-    # universidad_kenedy = PostgresOperator(
-    # task_id="universidad_kenedy",
-    # postgres_conn_id="postgres_default",
-    # sql="sql/query_kenedy.sql",
-    # )
-
-    """usar PostgresOperator ejecutar query
-    universidad_lationamericana = PostgresOperator(
-    task_id="universidad_sociales",
-    postgres_conn_id="postgres_default",
-    sql="sql/query_latinoamericana.sql",
-    )"""   
-
 
     tarea_1= DummyOperator(task_id='universidad_sociales') 
     tarea_2= DummyOperator(task_id='universidad_kenedy')
