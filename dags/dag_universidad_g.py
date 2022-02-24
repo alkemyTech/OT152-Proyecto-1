6759af5320c9959904b608bbc20aef27c20bff39
Gian Arias <ariasgian@gmail.com>
2022-02-23 21:47:37 -0300

    cut description and add comma

--- dags/dag_universidad_g.py
+++ dags/dag_universidad_g.py
@@ -5,9 +5,9 @@ from airflow.operators.dummy import DummyOperator
 
 with DAG(
     'universidades',
-    description= 'ETL para dos univ Facultad Latinoamericana De Ciencias Sociales yUniversidad J. F. Kennedy'
+    description= 'ETL Facultad Latinoamericana De Ciencias Sociales y Universidad J. F. Kennedy',
     schedule_interval= "@hourly", #funciona cada hora
-    start_date=datetime(2022, 2, 17)
+    start_date=datetime(2022, 2, 17),
 ) as dag:
 
     tarea_1= DummyOperator(task_id='universidad_sociales') 
