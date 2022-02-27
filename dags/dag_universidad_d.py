3662dc6f500d1220c06b72d39636820b412250d1
Gian Arias <ariasgian@gmail.com>
2022-02-27 00:21:55 -0300

    update setup database

--- dags/dag_universidad_d.py
+++ dags/dag_universidad_d.py
@@ -3,9 +3,9 @@ from datetime import timedelta, datetime
 from time import strftime
 from airflow.operators.python_operator import PythonOperator
 from airflow.models import DAG
-#from airflow.operators import python_operator
+from airflow.operators.bash_operator import BashOperator
 import sqlalchemy as db
-from os import environ
+from os import environ, getcwd
 from dotenv import load_dotenv
 import pandas as pd
 
@@ -21,7 +21,7 @@ args = {
     
 }
 #Configuracion de BD
-load_dotenv(dotenv_path='template.env') #set file .env
+load_dotenv(dotenv_path='./airﬂow/dags/OT152-Proyecto-1/template.env') #set file .env
 DB_USER = environ['DB_USER']
 DB_PASSWORD =environ['DB_PASSWORD']
 DB_HOST = environ['DB_HOST']
@@ -36,21 +36,30 @@ dag = DAG(
     schedule_interval=timedelta(minutes=5),
     start_date= datetime(2022,2,24)
     )
-
+def test():
+    with open("/airﬂow/dags/OT152-Proyecto-1/template.env") as f:
+        query = f.read()
+    f.close()
 
 def etl_extract():
-    with open('./sql/query_utn.sql') as f:
+    with open('/airﬂow/dags/OT152-Proyecto-1/sql/query_utn.sql') as f:
         query = f.read()
     f.close()
     con = db.create_engine(path, echo=True)
     df_raw= pd.read_sql_query(query, con)
-    df_raw.to_csv('./csv/data_utn.csv')
-    logging.info('test')
+    df_raw.to_csv('/airﬂow/dags/OT152-Proyecto-1/csv/data_utn.csv')
+    logging.info(getcwd())
     return df_raw
 
-df_raw=etl_extract()
+#df_raw=etl_extract()
 task_1= PythonOperator(
     task_id='extract_utn',
     python_callable=etl_extract,
     dag=dag
 )
+task_2=BashOperator(
+    task_id='bash',
+    bash_command=getcwd(),
+    dag=dag
+)
+[task_1]
