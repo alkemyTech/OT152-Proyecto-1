70c47ba7b4a885c9178b75714005fe8935f07132
Gian Arias <ariasgian@gmail.com>
2022-02-27 21:59:40 -0300

    setup config database

--- dags/dag_universidad_d.py
+++ dags/dag_universidad_d.py
@@ -3,16 +3,16 @@ from datetime import timedelta, datetime
 from time import strftime
 from airflow.operators.python_operator import PythonOperator
 from airflow.models import DAG
-from airflow.operators.bash_operator import BashOperator
-import sqlalchemy as db
-from os import environ, getcwd
+from os import environ, getcwd, path
 from dotenv import load_dotenv
 import pandas as pd
+import sqlalchemy as db
 
 logging.basicConfig(level=logging.INFO, datefmt=strftime("%Y-%m-%d"),
                     format='%(asctime)s - %(name)s - %(message)s')
 
 logger = logging.getLogger('Universidad_d')
+folder = path.abspath(path.join(path.dirname( __file__ ), '..'))
 
 #configuro los retries acorde a lo que pide la tarea
 args = {
@@ -21,14 +21,17 @@ args = {
     
 }
 #Configuracion de BD
-load_dotenv(dotenv_path='./airﬂow/dags/OT152-Proyecto-1/template.env') #set file .env
-DB_USER = environ['DB_USER']
-DB_PASSWORD =environ['DB_PASSWORD']
-DB_HOST = environ['DB_HOST']
-PORT = environ['DB_PORT']
-DB_NAME = environ['DB_NAME']
-path ='postgresql://{}:{}@{}/{}'.format(DB_USER,DB_PASSWORD,DB_HOST, DB_NAME) #Path DB for connection
-con = db.create_engine(path, echo=True)
+def connection():
+    file=folder+ '/template.env'
+    load_dotenv(dotenv_path=file) #set file .env
+    DB_USER = environ.get('DB_USER')
+    DB_PASSWORD =environ.get('DB_PASSWORD')
+    DB_HOST = environ.get('DB_HOST')
+    PORT = environ.get('DB_PORT')
+    DB_NAME = environ.get('DB_NAME')
+    path ='postgresql://{}:{}@{}:{}/{}'.format(DB_USER,DB_PASSWORD,DB_HOST, PORT, DB_NAME) #Path DB for connection
+    con = db.create_engine(path, echo=True)
+    return con
 
 dag = DAG(
     dag_id='dag_universidad_d', 
@@ -36,20 +39,20 @@ dag = DAG(
     schedule_interval=timedelta(minutes=5),
     start_date= datetime(2022,2,24)
     )
-def test():
-    with open("/airﬂow/dags/OT152-Proyecto-1/template.env") as f:
-        query = f.read()
-    f.close()
 
 def etl_extract():
-    with open('/airﬂow/dags/OT152-Proyecto-1/sql/query_utn.sql') as f:
+    file=folder+ '/sql/query_utn.sql'
+     
+    with open(file) as f:
         query = f.read()
     f.close()
-    con = db.create_engine(path, echo=True)
+    con=connection()
+    logging.info(path)
     df_raw= pd.read_sql_query(query, con)
-    df_raw.to_csv('/airﬂow/dags/OT152-Proyecto-1/csv/data_utn.csv')
-    logging.info(getcwd())
-    return df_raw
+    #df_raw.to_csv('./OT152-Proyecto-1/csv/data_utn.csv')
+    logging.info(query)
+    
+    #return df_raw
 
 #df_raw=etl_extract()
 task_1= PythonOperator(
@@ -57,9 +60,5 @@ task_1= PythonOperator(
     python_callable=etl_extract,
     dag=dag
 )
-task_2=BashOperator(
-    task_id='bash',
-    bash_command=getcwd(),
-    dag=dag
-)
+
 [task_1]
