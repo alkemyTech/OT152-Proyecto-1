7b063cc660b03e63c168766a272186ee37c4bec6
Gian Arias <ariasgian@gmail.com>
2022-02-28 11:09:17 -0300

    update fuction docstring and comment

--- dags/dag_universidad_d.py
+++ dags/dag_universidad_d.py
@@ -12,6 +12,8 @@ logging.basicConfig(level=logging.INFO, datefmt=strftime("%Y-%m-%d"),
                     format='%(asctime)s - %(name)s - %(message)s')
 
 logger = logging.getLogger('Universidad_d')
+
+#configura path raiz para mover dentro del proyecto
 folder = path.abspath(path.join(path.dirname( __file__ ), '..'))
 
 #configuro los retries acorde a lo que pide la tarea
@@ -22,37 +24,64 @@ args = {
 }
 #Configuracion de BD
 def connection():
+    """
+    Crea la conexion de la BD postgres segun las credeciales del archivo template.env que debe estar de manera local
+    Args:
+        None
+    Return:
+        Engine Instance: retorna la conexion
+    """
+    #carga las credenciales en template.env
     file=folder+ '/template.env'
-    load_dotenv(dotenv_path=file) #set file .env
+    load_dotenv(dotenv_path=file)
     DB_USER = environ.get('DB_USER')
     DB_PASSWORD =environ.get('DB_PASSWORD')
     DB_HOST = environ.get('DB_HOST')
-    PORT = environ.get('DB_PORT')
+    DB_PORT = environ.get('DB_PORT')
     DB_NAME = environ.get('DB_NAME')
-    path ='postgresql://{}:{}@{}:{}/{}'.format(DB_USER,DB_PASSWORD,DB_HOST, PORT, DB_NAME) #Path DB for connection
+
+    #Genera la conexion de la base de datos segun las credenciales
+    path =f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
     con = db.create_engine(path, echo=True)
     return con
+
+def extract(name_query,name_csv):
+    """
+    Lee query localizada en path /sql y genera archivo .csv 
+    
+    Args:
+        name_query(str): nombre de la query localizada en path /sql
+        name_csv(str): nombre que se le va a dar a csv generado
+    
+    Return:
+        file: csv generado en la ruta /csv/name_csv
+    """
+    with open(f'{folder}/sql/{name_query}.sql') as f:
+        query = f.read()
+    f.close()
+    con=connection()
+    df_raw= pd.read_sql_query(query, con)
+    df_raw.to_csv(f'{folder}/csv/{name_csv}.csv')
+
+def crear_csv():
+    """
+    Ejecuta la extraccion de las universidades UTN y Tres de Febrero
+    
+    Args:
+        None
+    Return:
+        /csv/csv_utn.csv
+        /csv/csv_tres_de_febrero.csv
+    """
+    extract('query_utn','csv_utn')
+    extract('query_tres_de_febrero','csv_tres_de_febrero')
 
 dag = DAG(
     dag_id='dag_universidad_d', 
     default_args=args,
     schedule_interval=timedelta(minutes=5),
     start_date= datetime(2022,2,24)
-    )
-
-def extract(name_query,name_csv):
-    file=folder+ '/sql/'+ name_query+'.sql'
-    with open(file) as f:
-        query = f.read()
-    f.close()
-    con=connection()
-    df_raw= pd.read_sql_query(query, con)
-    df_raw.to_csv(folder+ '/csv/' + name_csv+'.csv')
-
-def crear_csv():
-    extract('query_utn','csv_utn')
-    extract('query_tres_de_febrero','csv_tres_de_febrero')
-    
+    ) 
 task_1= PythonOperator(
     task_id='crear_csv',
     python_callable=crear_csv,
