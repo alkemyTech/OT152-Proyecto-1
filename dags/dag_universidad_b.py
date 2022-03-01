566a11b8586ede6d88b4bea138d77d261b568473
Tadeo Vardánega <tadeo.vardanega@gmail.com>
2022-03-01 18:56:37 -0300

    agrego los python operators b

--- dags/dag_universidad_b.py
+++ dags/dag_universidad_b.py
@@ -1,7 +1,13 @@
 import logging
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
+from decouple import config
+from sqlalchemy import engine
+from os import path
+import pandas as pd
+import sqlalchemy 
 
 
 logging.basicConfig(
@@ -16,6 +22,30 @@ default_args = {
     'retry_delay':timedelta(minutes=5)
 }
 
+#Configuro la conexion a la base de datos
+DB_USER = config('DB_USER')
+DB_PASSWORD = config('DB_PASSWORD')
+DB_HOST = config('DB_HOST')
+DB_PORT = config('DB_PORT')
+DB_NAME = config('DB_NAME')
+DB_TYPE = config('DB_TYPE')
+
+conexion = f'{DB_TYPE}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+engine = sqlalchemy.create_engine(conexion)
+conn = engine.connect()
+
+
+#Extraigo las tablas de las consultas y las guardo en un csv
+root_folder = path.abspath(path.join(path.dirname(__file__), '..'))
+
+def save_to_csv(university):
+    file = open(f'{root_folder}/sql/{university}.sql', "r")
+    query = sqlalchemy.text(file.read())
+    data = pd.read_sql_query(query, conn)
+
+    data.to_csv(f'{root_folder}/csv/universidad_{university}.csv', index=False)
+
+
 with DAG(
     'Universities_B_dags',
     description='Ejecuta ETL de las universidades B',
@@ -25,6 +55,8 @@ with DAG(
     # Tareas a ejecutar leyendo sql con pandas
     tarea1 = DummyOperator(task_id='ETL_comahue')
     tarea2 = DummyOperator(task_id='ETL_salvador')
+    save_to_csv = PythonOperator(task_id='Universidades_B_data', python_callable=save_to_csv, dag=dag)
+
 
     # Orden de tareas
     tarea1 >> tarea2
