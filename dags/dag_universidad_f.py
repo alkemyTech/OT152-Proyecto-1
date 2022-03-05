a768f45816c9849e96e8beb3d99a3fa1df17d7af
Joaq33 <joakoillia@gmail.com>
2022-03-04 21:35:13 -0300

    fix encapsulamiento de funciones y gitignore

--- dags/dag_universidad_f.py
+++ dags/dag_universidad_f.py
@@ -1,7 +1,7 @@
 from datetime import timedelta, datetime
 import logging
 from decouple import config
-from os import path
+from os import path, makedirs
 
 import sqlalchemy
 
@@ -14,20 +14,26 @@ from airflow.operators.python import PythonOperator
 logging.basicConfig(level=logging.INFO, datefmt='%Y-%m-%d', format='%(asctime)s - %(name)s - %(message)s')
 logger = logging.getLogger('univ_f')
 
-# Connection DB credentials
-DB_USER = config('DB_USER')
-DB_PASSWORD = config('DB_PASSWORD')
-DB_HOST = config('DB_HOST')
-DB_PORT = config('DB_PORT')
-DB_NAME = config('DB_NAME')
 
-# Initialize connection
-db_url = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-engine = sqlalchemy.create_engine(db_url)
-conn = engine.connect()
+def init_connection():
+    """
+    Initializes connection to postgres db
+    Returns:
+        MockConnection
+    """
+    # Connection DB credentials
+    DB_USER = config('DB_USER')
+    DB_PASSWORD = config('DB_PASSWORD')
+    DB_HOST = config('DB_HOST')
+    DB_PORT = config('DB_PORT')
+    DB_NAME = config('DB_NAME')
 
-# Set root folder path
-root_folder = path.abspath(path.join(path.dirname(__file__), ".."))
+    # Initialize connection
+    db_url = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    engine = sqlalchemy.create_engine(db_url)
+    conn = engine.connect()
+
+    return conn
 
 
 def extract_data(university):
@@ -39,10 +45,25 @@ def extract_data(university):
     Returns:
         None
     """
+    conn = init_connection()
+
+    root_folder = get_root_folder()
     with open(f"{root_folder}/sql/query_{university}.sql", "r") as f:
         sql = f.read()
         response = pd.read_sql_query(sql, conn)
+        if not path.exists(f'{root_folder}/csv'):
+            makedirs(f'{root_folder}/csv')
         response.to_csv(f'{root_folder}/csv/universidad_{university}.csv', index=False)
+
+
+def get_root_folder():
+    """
+    Set root folder path
+    Returns:
+        String
+    """
+    root_folder = path.abspath(path.join(path.dirname(__file__), ".."))
+    return root_folder
 
 
 default_args = {
