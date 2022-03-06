2bcc7d7cf720438f7ccf891425f3cb09c234db06
penalozale <penaloza.le@gmail.com>
2022-03-06 12:24:39 -0300

    query PyOperator fixed

--- dags/dag_universidad_a.py
+++ dags/dag_universidad_a.py
@@ -10,43 +10,48 @@ from decouple import config
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
 
-## Database information
-user = config("DB_USER")
-password = config("DB_PASSWORD")
-host = config("DB_HOST")
-port = config("DB_PORT")
-database = config("DB_NAME")
+def DB_connection():
+    '''
+    Connect to a Database
+    '''
+    ## DB information
+    user = config("DB_USER")
+    password = config("DB_PASSWORD")
+    host = config("DB_HOST")
+    port = config("DB_PORT")
+    database = config("DB_NAME")
+    
+    ## DB connection
+    db_url = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
+    engine = create_engine(db_url)
 
-## Database connection
-db_url = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
-engine = create_engine(db_url)
-
-## SQL queries file paths (if you are in: "OT152-Proyecto-1" or "dags" directory)
-if 'proyecto/dags' in os.getcwd():
-    project_folder = os.path.normpath(os.getcwd() + os.sep + os.pardir)
-    u_flores = os.path.join(project_folder, 'sql', 'query_flores.sql')
-    u_villa_maria = os.path.join(project_folder, 'sql', 'query_villa.sql')
-else:
-    project_folder = os.getcwd()
-    u_flores = os.path.join(project_folder, 'sql', 'query_flores.sql')
-    u_villa_maria = os.path.join(project_folder, 'sql', 'query_villa.sql')
+    return(engine_connect())
 
 ## SQL query to csv file
 def query_csvfile(**kwargs):
     '''
     Connet to a DB, execute a sql query and save the data in a .csv file
-    INPUT: kwargs['file_path'] = path where we can find the .sql file
+    INPUT: kwargs['sql_file'] = .sql file name (query file)
     OUTPUT: kwargs['file_name'] = name of .csv file with data table.    
     '''
-    with engine.connect() as conn:
-        path = open(kwargs['file_path'])
-        query = text(path.read())
+    conn = DB_connection()
+
+    # root folder
+    root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    ## Create 'csv' folder if not exist
+    new_folder = os.path.join(root_dir, 'csv')
+    os.makedirs(new_folder, exist_ok=True)
+
+    file_path = os.path.join(root_dir,'sql',kwargs['sql_file'])
+    with open(file_path) as file:
+        query = text(file.read())
         result = conn.execute(query)
         df = pd.DataFrame(result.fetchall())
         df.columns = result.keys()
-        csv_path = os.path.join(project_folder, 'csv', kwargs['file_name'])
+        csv_path = os.path.join(root_dir, 'csv', kwargs['file_name'])
         df.to_csv(csv_path, sep = ',', index=False)
 
+# DAGs arguments
 default_args = {
     'retries': 5, # Quantity of retries before shutdown
     'retry_delay': timedelta(minutes=5) # Wait time before next retry
@@ -69,7 +74,7 @@ with DAG(
     # Tasks Univerdisdad las Flores
     sql_universidad_flores = PythonOperator(task_id='sql_universidad_flores',
                                 python_callable = query_csvfile,
-                                op_kwargs = {'file_path':'u_flores',
+                                op_kwargs = {'sql_file':'query_flores.sql',
                                 'file_name':'flores.csv'})
     processing_universidad_flores = DummyOperator(task_id='processing_universidad_flores')
     load_universidad_flores = DummyOperator(task_id='load_universidad_flores')
@@ -77,7 +82,7 @@ with DAG(
     # Tasks Universidad Nacional de Villa Maria
     sql_universidad_villa_maria = PythonOperator(task_id='ELT_Universidad_Nacional_De_Villa_Maria',
                                     python_callable = query_csvfile,
-                                    op_kwargs = {'file_path':'u_villa_maria',
+                                    op_kwargs = {'sql_file':'query_villa.sql',
                                     'file_name':'villa_maria.csv'})
     processing_universidad_villa_maria = DummyOperator(task_id='processing_universidad_villa_maria')
     load_universidad_villa_maria = DummyOperator(task_id='load_universidad_villa_maria')
