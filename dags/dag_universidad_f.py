444765c9f8a8cf86cb00f4585ded205aa3d83e66
Joel Solaligue <solaligue.joel98@gmail.com>
2022-03-07 20:57:39 -0300

    Funciont pandas and pythonOperator

--- dags/dag_universidad_f.py
+++ dags/dag_universidad_f.py
@@ -1,7 +1,10 @@
+from ast import Str
 from datetime import timedelta, datetime
 import logging
+from operator import index
+from unittest.util import strclass
 from decouple import config
-from os import path
+from os import makedirs, mkdir, path
 import psycopg2
 from dotenv import load_dotenv
 
@@ -21,82 +24,94 @@ logger = logging.getLogger('univ_f')
 
 
 root_folder = path.abspath(path.join(path.dirname(__file__), ".."))
-file = root_folder +'/template.env'
 
-load_dotenv(dotenv_path=file)
 
-def db_connection():
-    server = os.getenv('SERVER')
-    host = os.getenv('HOST')
-    db_user = os.getenv('DB_USER')
-    db_password = os.getenv('DB_PASSWORD')
-    db = os.getenv('DB')
+        
+def clean_string(x):
+        return x.lower().replace("-", "").strip()
     
-    conexion = psycopg2.connect(host=server, database=db,port=host,user=db_user,password=db_password)
-    return conexion    
-    
-
-def extract(name_query,):
-    df = pd.read_csv(f'{root_folder}/csv/')
-    return df
-
-def transform_moron(df_moron):
+def _transform_moron():
     df_moron = pd.read_csv(f'{root_folder}/csv/moron.csv')
-    df_moron.rename({'universidad':'university','carrerra':'carer',
-               'fechaiscripcion':'inscription_date',
+    
+    if not path.exists(f'{root_folder}/txt'):
+        makedirs(f'{root_folder}/txt')
+        
+    df_moron.rename(columns={'universidad':'university','carrerra':'carer',
+               'fechaiscripccion':'inscription_date',
                'sexo':'gender','codgoposstal':'postal_code',
                'eemail':'email'},inplace=True)
+    
+
     
     #Data processing
     df_moron['nombrre'] = df_moron['nombrre'].str.replace(' ','_')
     name = df_moron['nombrre'].str.split('_',expand=True)
     df_moron['first_name'] = name[0]
     df_moron['last_name'] = name[1]
-    df_moron['gender'] = df_moron['gender'].replace(['M','F'],['male','female'])
-
-    location = pd.read_csv(f'{root_folder}/csv/codigos_postales.csv')
-    df_moron['location'] = location['localidad']
-    df_moron.columns = [x.lower().replace(' ','_').strip("_") for x in df_moron.columns]
     
+    gender_female = df_moron['gender'] =='F'
+    gender_male = df_moron['gender'] =='M'
+    
+    df_moron.loc[gender_female, 'gender'] = 'female'
+    df_moron.loc[gender_male, 'gender'] = 'male'
+    
+    df_moron.university = df_moron['university'].apply(clean_string)
+    df_moron.carer = df_moron['carer'].apply(clean_string)
+    df_moron.inscription_date = df_moron['inscription_date'].apply(clean_string)
+    df_moron.first_name = df_moron['first_name'].apply(clean_string)
+    df_moron.last_name = df_moron['last_name'].apply(clean_string)
+    df_moron.email = df_moron['email'].apply(clean_string)    
+        
+    location = pd.read_csv(f'{root_folder}/csv/codigos_postales.csv')
+    df_moron['location'] = location['localidad'].apply(lambda x: x.lower().replace(" ",''))
+
     #Removing extra columns
     df_moron.drop(columns=['nombrre'],inplace=True)
+    #Save to txt
+    df_moron.to_csv(f'{root_folder}/txt/moron.txt',index=None)        
 
+def _transform_rio_cuarto():
+    df_rio_cuarto = pd.read_csv(f'{root_folder}/csv/rio_cuarto.csv')
+    
+    if not path.exists(f'{root_folder}/txt'):
+        makedirs(f'{root_folder}/txt')
         
-    return df_moron
-
-def transform_rio_cuarto(df_rio_cuarto):
-    df_rio_cuarto.rename({'univiersities':'university','carrera':'carer',
-               'inscriptions_dates':'inscription_date',
+    df_rio_cuarto.rename(columns={'univiersities':'university','carrera':'carer',
+               'inscription_dates':'inscription_date',
                'sexo':'gender','localidad':'location'},inplace=True)
     
-    df_rio_cuarto['gender'] = df_rio_cuarto['gender'].replace(['M','F'],['male','female'])
+    gender_female = df_rio_cuarto['gender'] =='F'
+    gender_male = df_rio_cuarto['gender'] =='M'
+    
+    df_rio_cuarto.loc[gender_female, 'gender'] = 'female'
+    df_rio_cuarto.loc[gender_male, 'gender'] = 'male'   
     
     #Clear columns df_rio_cuarto
     df_rio_cuarto['names'] = df_rio_cuarto['names'].str.replace('-','_')
     name = df_rio_cuarto['names'].str.split('_',expand=True)
     df_rio_cuarto['first_name'] = name[0]
     df_rio_cuarto['last_name'] = name[1]
-    df_rio_cuarto.columns = [x.lower().replace(' ','_').strip("_") for x in df_rio_cuarto]
     
+    df_rio_cuarto.university = df_rio_cuarto['university'].apply(clean_string)
+    df_rio_cuarto.carer = df_rio_cuarto['carer'].apply(clean_string)
+    df_rio_cuarto.inscription_date = df_rio_cuarto['inscription_date'].apply(clean_string)
+    df_rio_cuarto.first_name = df_rio_cuarto['first_name'].apply(clean_string)
+    df_rio_cuarto.last_name = df_rio_cuarto['last_name'].apply(clean_string)
+    df_rio_cuarto.location = df_rio_cuarto['location'].apply(clean_string)
+    df_rio_cuarto.email = df_rio_cuarto['email'].apply(clean_string)
+    
+    
+    postal_code = pd.read_csv(f'{root_folder}/csv/codigos_postales.csv')
+    df_rio_cuarto['postal_code'] = postal_code['codigo_postal']
+    
+
     #Removin extra columns
     df_rio_cuarto.drop(columns=['names'],inplace=True)
+    
+    #Save to txt
+    df_rio_cuarto.to_csv(f'{root_folder}/txt/rio_cuarto.txt',index=None)
  
-    
-def load(df,file):
-    df.to_csv(f'{root_folder}/txt/{file}.txt',index=None)
 
-def _etl():
-    moron_csv = extract('query_moron','%D/%m/%Y')
-
-    df_moron = transform_moron(moron_csv)
-    load(df_moron,'txt_moron')
-    
-    rio_cuarto_csv = extract('query_rio_cuarto','%D/%m/%Y')
-    df_rio_cuarto = transform_rio_cuarto(rio_cuarto_csv)
-    load(df_rio_cuarto,'txt_rio_cuarto')
-    
-    
-    
 default_args = {
     "retries": 5,  # try 5 times
     "retry_delay": timedelta(minutes=10)  # wait 10 minutes to try again
@@ -113,12 +128,18 @@ with DAG(
     tarea2 = DummyOperator(task_id='Query_F2') #Universidad de Río Cuarto
     tarea3 = DummyOperator(task_id='Processing_data')#Reading and processing data using read_sql
     tarea4 = DummyOperator(task_id='Upload_S3')#uploading data to s3
-    [tarea1, tarea2] >> tarea3 >> tarea4
     
     
-    create_txt = PythonOperator(
-        task_id='create_txt',
-        python_callable=_etl,
+    moron_txt = PythonOperator(
+        task_id='moron_txt',
+        python_callable=_transform_moron,
         dag=dag
     )
-    create_txt
+    rio_cuarto_txt = PythonOperator(
+        task_id='rio_cuarto_txt',
+        python_callable=_transform_rio_cuarto,
+        dag=dag
+    )
+    [tarea1>>moron_txt, tarea2 >> rio_cuarto_txt] >> tarea4
+
+    
