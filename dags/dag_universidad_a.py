f2fe5c9da4218a5c52ef5b27ce9dc147fb1a23f8
Tadeo Vardánega <tadeo.vardanega@gmail.com>
2022-03-09 12:14:50 -0300

    Actualice el archivo

--- dags/dag_universidad_a.py
+++ dags/dag_universidad_a.py
@@ -1,10 +1,13 @@
 from datetime import datetime, timedelta
+from genericpath import exists
+from importlib.resources import path
 from multiprocessing import connection
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
 import logging
 from distutils.debug import DEBUG
+from os import path, makedirs 
 import os
 import pandas as pd
 from decouple import config
@@ -12,6 +15,64 @@ from pendulum import today
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
 import numpy as np
+
+#Ruta de la carpeta root
+root_folder = path.abspath(path.join(path.dirname(__file__), ".."))
+
+#Limpio cualquier espacio innecesario
+def clean_string(df):
+    df = df.str.lower().str.replace('_', ' ').str.strip()
+    return df
+
+#Transformo el csv de flores a txt
+def _transform_flores():
+    df_flores = pd.read_csv(f'{root_folder}/csv/flores.csv')
+    if not path.exists(f'{root_folder}/txt'):
+        makedirs(f'{root_folder}/txt')
+
+
+    #Procesameinto de los datos de la universidad de flores
+    df_flores['university'] = clean_string['university']
+    df_flores['career'] = clean_string['career']
+    df_flores['first_name'] = clean_string['first_name']
+    df_flores['last_name'] = clean_string['last_name']
+    df_flores['age'] = clean_string['age']
+    df_flores['location'] = clean_string['location']
+    df_flores['email'] = clean_string['email']
+    df_flores['inscription_date'] = clean_string['inscription_date']
+    df_flores['gender'] = ['gender'].str.replace('M', 'male')
+    df_flores['gender'] = ['gender'].str.replace('F', 'female')
+
+
+    #Lo guardo en un .txt
+    df_flores.to_csv(f'{root_folder}/txt/flores.txt', index=None)
+
+
+#Transformo el csv de villa maria a txt
+def _transform_villa_maria():
+    df_villa_maria = pd.read_csv(f'{root_folder}/csv/villa.csv')
+    if not path.exists(f'{root_folder}/txt'):
+        makedirs(f'{root_folder}/txt')
+
+    
+    #Procesamiento de los datos de villa maria
+    df_villa_maria['university'] = clean_string['university']
+    df_villa_maria['career'] = clean_string['career']
+    df_villa_maria['first_name'] = clean_string['first_name']
+    df_villa_maria['last_name'] = clean_string['last_name']
+    df_villa_maria['age'] = clean_string['age']
+    df_villa_maria['location'] = clean_string['location']
+    df_villa_maria['email'] = clean_string['email']
+    df_villa_maria['inscription_date'] = clean_string['inscription_date']
+
+    df_villa_maria['gender'] = ['gender'].str.replace('M', 'male')
+    df_villa_maria['gender'] = ['gender'].str.replace('F', 'female')
+    codigo_postal = pd.read_csv(f'{root_folder}/csv/codigos_postales.csv', 
+                                dtype={'codigo_postal': 'str'})
+    df_villa_maria = df_villa_maria.merge(codigo_postal, on = 'codigo_postal')
+
+    #Lo guardo en un .txt
+    df_villa_maria.to_csv(f'{root_folder}/txt/villa.txt', index=None)
 
 ## Database information
 user = config("DB_USER")
@@ -60,44 +121,6 @@ def extract(name_queary, format_date):
     return df_raw
 
 
-def clean_string(df):
-    df = df.str.lower().str.replace('_', ' ').str.strip()
-    return df
-
-def transform(df):
-
-    df['university'] = clean_string(df['university'])
-    df['career'] = clean_string(df['career'])
-    df['first_name'] = clean_string(df['first_name'])
-    df['last_name'] = clean_string(df['last_name'])
-    df['age'] = clean_string(df['age'])
-    df['location'] = clean_string(df['location'])
-    df['email'] = clean_string(df['email'])
-    df['inscription_date'] = clean_string(df['inscription_date'])
-
-    df['gender'] = df['gender'].str.replace('M', 'male')
-    df['gender'] = df['gender'].str.replace('F', 'female')
-
-    if 'codigo_postal' in df.columns:
-        df_postal = pd.read_csv(f'{project_folder}/csv/codigos_postales.csv', 
-                                dtype={'codigo_postal': 'str'})
-        df = df.merge(df_postal, on = 'codigo_postal')
-        return df
-
-def load (df, file):
-    df.to_csv(f'{project_folder}/txt/{file}.txt')
-
-def etl():
-    df_raw = extract('query_flores', '%Y/%m/%D')
-    df = transform(df_raw)
-    load(df, 'txt_flores')
-
-
-    df_raw = extract('query_villa', '%D/%m/%Y')
-    df = transform(df_raw)
-    load(df, 'txt_villa')
-
-
 default_args = {
     'retries': 5, # Quantity of retries before shutdown
     'retry_delay': timedelta(minutes=5) # Wait time before next retry
@@ -122,7 +145,11 @@ with DAG(
                                 python_callable = query_csvfile,
                                 op_kwargs = {'file_path':'u_flores',
                                 'file_name':'flores.csv'})
-    processing_universidad_flores = DummyOperator(task_id='processing_universidad_flores')
+    processing_universidad_flores = PythonOperator(
+        task_id='processing_universidad_flores',
+        python_callable=_transform_flores,
+        dag=dag
+    )
     load_universidad_flores = DummyOperator(task_id='load_universidad_flores')
 
     # Tasks Universidad Nacional de Villa Maria
@@ -130,7 +157,11 @@ with DAG(
                                     python_callable = query_csvfile,
                                     op_kwargs = {'file_path':'u_villa_maria',
                                     'file_name':'villa_maria.csv'})
-    processing_universidad_villa_maria = DummyOperator(task_id='processing_universidad_villa_maria')
+    processing_universidad_villa_maria = PythonOperator(
+        task_id='processing_universidad_villa_maria',
+        python_callable=_transform_villa_maria,
+        dag=dag
+    )
     load_universidad_villa_maria = DummyOperator(task_id='load_universidad_villa_maria')
 
     # Tasks order
