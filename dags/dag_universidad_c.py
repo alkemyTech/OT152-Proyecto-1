ea58e2be2d6108d2eb0a5d7c4df81eccf806619e
Gian Arias <ariasgian@gmail.com>
2022-03-08 21:29:20 -0300

    clean transform function

--- dags/dag_universidad_c.py
+++ dags/dag_universidad_c.py
@@ -20,19 +20,18 @@ def transform(df):
     """
     folder = path.abspath(path.join(path.dirname( __file__ ), '..'))
     columnas= ['universidad','carrera', 'email','first_name', 'last_name']
-    #df['universidad']=df['universidad'].str.lower().str.replace('_',' ').str.strip()
-    #df['carrera']= df['carrera'].str.lower().str.replace('_',' ').str.strip()
-    #df['email']= df['email'].str.lower().str.replace('_',' ').str.strip()
-        
+            
     #separa el nombre en dos columnas
     df['nombre']=df['nombre'].str.replace(' ','_') #normaliza el nombre con "_"
     nombre=df['nombre'].str.split('_', expand=True)
     df['first_name']= nombre[0]
     df['last_name']= nombre[1]
     df.drop(columns=['nombre'],inplace=True)
-    df['first_name']= limpiar_string(df['first_name'])
-    df['last_name']=limpiar_string(df['last_name'])
-    df[columnas]=df[columnas].str.lower().str.replace('_',' ').str.strip()
+    
+    # actualiza las columnas en minusculas y sin espacios
+    for columna in columnas:
+        df[columna]=df[columna].str.lower().str.replace('_',' ').str.strip()
+    
     #convertir sexo m=male y f=female
     df['sexo']=df['sexo'].str.replace('m', 'male')
     df['sexo']=df['sexo'].str.replace('f', 'female')
@@ -47,7 +46,6 @@ def transform(df):
         df_postal=pd.read_csv(f'{folder}/csv/codigos_postales.csv',
                               dtype={'codigo_postal':'str'})
         df=df.merge(df_postal, on='codigo_postal')
-    logging.info(df)
     return df
 def load_txt():
     #extrae csv de universidad de Palermo
@@ -57,7 +55,7 @@ def load_txt():
     df.to_csv(f'{folder}/txt/txt_palermo.txt')
 
     #extrae csv de universidad de UTN
-    df_raw= pd.read_csv(f'{folder}/csv/query_utn.csv')
+    df_raw= pd.read_csv(f'{folder}/csv/query_utn.csv', infer_datetime_format=True )
     df = transform(df_raw)
     df.to_csv(f'{folder}/txt/txt_nacional.txt')
     
