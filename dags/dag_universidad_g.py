80e79b89062d12582f28b2147d5b6a108cf01c8a
SolaligueJoel <81047856+SolaligueJoel@users.noreply.github.com>
2022-03-13 21:13:16 -0300

    python operator implementation, group g (#43)
    * Adding files to gitignore
    
    * Implementing pythonoperator for group universities G
    
    * renaming csv files
    
    * Adding files: .txt, .csv
    
    * Modifying files
    
    * Adding fixes
    
    * delete dotenv
    
    Co-authored-by: julianpalmerio <julianpalmerio@hotmail.com>

--- dags/dag_universidad_g.py
+++ dags/dag_universidad_g.py
@@ -1,9 +1,38 @@
-
-from datetime import datetime
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
 import logging
-from datetime import timedelta
+from decouple import config
+
+import psycopg2
+from datetime import datetime,timedelta
+import pandas as pd
+from os import path
+
+#main folder location
+folder = path.abspath(path.join(path.dirname( __file__ ), '..'))
+file = folder +'/template.env'
+
+
+def db_connection():
+    server = config('DB_HOST')
+    host = config('DB_PORT')
+    db_user = config('DB_USER')
+    db_password = config('DB_PASSWORD')
+    db = config('DB_NAME')
+    
+    conexion = psycopg2.connect(host=server, database=db,port=host,user=db_user,password=db_password)
+    return conexion
+
+def query_db(query,csv):
+     with open(f'{folder}/sql/{query}.sql') as f:
+        query = f.read()
+        f.close()
+        con=db_connection()
+        df_query= pd.read_sql_query(query, con)
+        df_query.to_csv(f'{folder}/csv/{csv}.csv',index=False)
+
+
 
 default_args = {
     "retries": 5,  # Try 5 times
@@ -17,6 +46,7 @@ logging.basicConfig(
         datefmt='%Y-%m-%d')
 logger = logging.getLogger('univ_g')
 
+
 with DAG(
     'dag_universidad_g',
     description= 'ETL univ Facultad Latinoamericana De Ciencias Sociales y Universidad J. F. Kennedy',
@@ -26,4 +56,18 @@ with DAG(
 
     tarea_1= DummyOperator(task_id='universidad_sociales') 
     tarea_2= DummyOperator(task_id='universidad_kenedy')
-    [tarea_1,tarea_2]
+    
+    csv_latinoamericana = PythonOperator(
+        task_id='create_csv_file',
+        python_callable=query_db,
+        op_args=['query_latinoamericana','latinoamericana'],
+        dag=dag
+    )
+    
+    csv_kennedy = PythonOperator(
+        task_id='csv_kennedy',
+        python_callable=query_db,
+        op_args=['query_kenedy','kenedy'],
+        dag=dag
+    )
+    [tarea_1 >> csv_latinoamericana],[tarea_2>>csv_kennedy]
