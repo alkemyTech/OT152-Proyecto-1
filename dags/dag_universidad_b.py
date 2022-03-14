af80029a68275324071611bc8cb2c86c386bad3f
Gian Arias <ariasgian@gmail.com>
2022-03-14 20:50:41 -0300

    update use decouple library

--- dags/dag_universidad_b.py
+++ dags/dag_universidad_b.py
@@ -1,62 +1,62 @@
+from os import path
 import logging
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
 from datetime import datetime, timedelta
-from os import environ, path
-from dotenv import load_dotenv
+from decouple import config
 from airflow.operators.python_operator import PythonOperator
 import boto3
-from botocore.exceptions import ClientError
 
 # Para moverme por los diferentes directorio
-folder = path.abspath(path.join(path.dirname( __file__ ), '..'))
+folder = path.abspath(path.join(path.dirname(__file__), '..'))
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(message)s',  
+    format='%(asctime)s - %(name)s - %(message)s',
     datefmt='%Y-%m-%d'
 )
 logger = logging.getLogger("univ_b")
 
-#Setting retries at 5
+# Setting retries at 5
 default_args = {
-    'retries':5,
-    'retry_delay':timedelta(minutes=5)
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5)
 }
+
 
 def load_s3():
     """
     sube archivo txt a repositorio s3
-        
     Args:
         None
-    
     Return:
         FIle load in s3 bucket
     """
     # cargas las credenciales  guardadas en template.env
-    file=folder+ '/template.env'
-    load_dotenv(dotenv_path=file)
-    aws_access_key_id=environ.get('AWS_ACCESS_KEY_ID')
-    aws_secret_access_key=environ.get('AWS_SECRET_ACCESS_KEY')
-    bucket_name = environ.get('BUCKET_NAME')
+    aws_access_key_id = config('AWS_ACCESS_KEY_ID')
+    aws_secret_access_key = config('AWS_SECRET_ACCESS_KEY')
+    bucket_name = config('BUCKET_NAME')
 
-    #carga el archivo
-    s3_client = boto3.client('s3', aws_access_key_id= aws_access_key_id,aws_secret_access_key=aws_secret_access_key)
+    # carga el archivo
+    s3_client = boto3.client(
+        's3', aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key
+        )
     with open(f'{folder}/txt/txt_comahue.txt', 'rb') as f:
-        s3_client.upload_fileobj(f, bucket_name,'txt_comahue.txt')
+        s3_client.upload_fileobj(f, bucket_name, 'txt_comahue.txt')
+
 
 with DAG(
     'Universities_B_dags',
     description='Ejecuta ETL de las universidades B',
-    schedule_interval=timedelta(hours=1), # Ejecución cada hora
-    start_date=(datetime(2022, 2, 18)) # Fecha de inicio
+    schedule_interval=timedelta(hours=1),  # Ejecución cada hora
+    start_date=(datetime(2022, 2, 18))  # Fecha de inicio
 ) as dag:
     # Tareas a ejecutar leyendo sql con pandas
     tarea1 = DummyOperator(task_id='ETL_comahue')
     tarea2 = DummyOperator(task_id='ETL_salvador')
-    cargar_s3= PythonOperator(
-    task_id='load_s3',
-    python_callable=load_s3,
-    dag=dag)
+    cargar_s3 = PythonOperator(
+        task_id='load_s3',
+        python_callable=load_s3,
+        dag=dag)
 
     # Orden de tareas
     tarea1 >> tarea2
