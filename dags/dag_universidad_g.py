26161f9515a2a1de6e00aded13562af053cd61b8
Tadeo Vardánega <tadeo.vardanega@gmail.com>
2022-03-14 14:12:38 -0300

    Pase el archivo por el linter de flake8

--- dags/dag_universidad_g.py
+++ dags/dag_universidad_g.py
@@ -1,12 +1,10 @@
-
-from datetime import datetime
-from airflow import DAG
-from airflow.operators.dummy import DummyOperator
 import logging
-from datetime import timedelta
-import boto3
 import os
 from os import path
+from datetime import datetime
+from airflow import DAG
+from datetime import timedelta
+import boto3
 from airflow.operators.python import PythonOperator
 
 default_args = {
@@ -21,31 +19,31 @@ logging.basicConfig(
         datefmt='%Y-%m-%d')
 logger = logging.getLogger('univ_g')
 
+
 def load_s3():
-    #Configuro la conexion con AWS 
+    # Configuro la conexion con AWS
     aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
     aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
     bucket_name = os.environ.get('BUCKET_NAME')
 
-    #Carga del archivo
+    # Carga del archivo
     root_folder = path.abspath(path.join(path.dirname(__file__), '..'))
-    s3_client = boto3.client('s3', aws_access_key_id= aws_access_key_id,aws_secret_access_key=aws_secret_access_key)
+    s3_client = boto3.client('s3',
+                             aws_access_key_id=aws_access_key_id,
+                             aws_secret_access_key=aws_secret_access_key)
     with open(f'{root_folder}/txt/txt_latinoamericana.txt', 'rb') as f:
-        s3_client.upload_fileobj(f, bucket_name,'txt_latinoamericana.txt')
+        s3_client.upload_fileobj(f, bucket_name, 'txt_latinoamericana.txt')
+
 
 with DAG(
     'dag_universidad_g',
-    description= 'ETL univ Facultad Latinoamericana De Ciencias Sociales y Universidad J. F. Kennedy',
-    schedule_interval= "@hourly", #funciona cada hora
+    description='ETL univ Facultad Latinoamericana De Ciencias Sociales y Universidad J. F. Kennedy',
+    schedule_interval="@hourly",  # funciona cada hora
     start_date=datetime(2022, 2, 17)
 ) as dag:
 
-    tarea_1= DummyOperator(task_id='universidad_sociales') 
-    tarea_2= DummyOperator(task_id='universidad_kenedy')
-    carga_de_s3= PythonOperator(
-    task_id='load_s3',
-    python_callable=load_s3,
-    dag=dag)
+    carga_de_s3 = PythonOperator(task_id='load_s3', python_callable=load_s3,
+                                 dag=dag)
 
-    [tarea_1,tarea_2]
-    carga_de_s3
+    # Ejecucion de la tarea
+    [carga_de_s3]
