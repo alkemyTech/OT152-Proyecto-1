362aa28676c9c4a6d8b738ffd24721b58650429f
Tadeo Vardánega <tadeo.vardanega@gmail.com>
2022-03-14 14:06:11 -0300

    Pase el archivo por el linter de flake8

--- dags/dag_universidad_a.py
+++ dags/dag_universidad_a.py
@@ -1,14 +1,9 @@
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
 import logging
-from distutils.debug import DEBUG
-import os
 import pandas as pd
 from decouple import config
-from sqlalchemy import create_engine
-from sqlalchemy.sql import text
 import sqlalchemy
 from os import path
 from os import remove
@@ -16,7 +11,7 @@ from os import mkdir
 
 
 def execute_queries(university):
-    #Configuro la conexion a la base de datos
+    # Configuro la conexion a la base de datos
     DB_USER = config('DB_USER')
     DB_PASSWORD = config('DB_PASSWORD')
     DB_HOST = config('DB_HOST')
@@ -27,25 +22,28 @@ def execute_queries(university):
     conexion = f'{DB_TYPE}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
     engine = sqlalchemy.create_engine(conexion)
     conn = engine.connect()
-    #Configuro un path para la carpeta root
+    # Configuro un path para la carpeta root
     root_folder = path.abspath(path.join(path.dirname(__file__), '..'))
 
     with open(f"{root_folder}/sql/query_{university}.sql", "r") as f:
         sql = f.read()
         response = pd.read_sql_query(sql, conn)
         if path.exists(f'{root_folder}/csv'):
-            remove(f'{root_folder}/csv') 
+            remove(f'{root_folder}/csv')
         mkdir(f'{root_folder}/csv')
-        response.to_csv(f'{root_folder}/csv/universidad_{university}.csv', index=False)
+        response.to_csv(f'{root_folder}/csv/universidad_{university}.csv',
+                        index=False)
+
 
 default_args = {
-    'retries': 5, # Quantity of retries before shutdown
+    'retries': 5,  # Quantity of retries before shutdown
     'retry_delay': timedelta(minutes=5) # Wait time before next retry
 }
 
+
 # create logger
-FORMAT='%(asctime)s - %(name)s - %(message)s'
-logging.basicConfig(level=logging.DEBUG,datefmt=("%Y-%m-%d"),format=FORMAT)
+FORMAT = '%(asctime)s - %(name)s - %(message)s'
+logging.basicConfig(level=logging.DEBUG, datefmt=("%Y-%m-%d"),format=FORMAT)
 logger = logging.getLogger('universidad_a')
 
 with DAG(
@@ -57,19 +55,18 @@ with DAG(
     start_date=(datetime(2022, 2, 18))
 
 ) as dag:
+
     # Tasks Univerdisdad las Flores
-    sql_universidad_flores = PythonOperator(task_id='Query_flores', python_callable=execute_queries, op_args=["flores"],
-                            dag=dag)
-    processing_universidad_flores = DummyOperator(task_id='processing_universidad_flores')
-    load_universidad_flores = DummyOperator(task_id='load_universidad_flores')
+    sql_universidad_flores = PythonOperator(task_id='Query_flores',
+                                            python_callable=execute_queries,
+                                            op_args=["flores"],
+                                            dag=dag)
 
     # Tasks Universidad Nacional de Villa Maria
-    sql_universidad_villa_maria = PythonOperator(task_id='Query_villa_maria', python_callable=execute_queries, op_args=["villa_maria"],
-                            dag=dag)
-    processing_universidad_villa_maria = DummyOperator(task_id='processing_universidad_villa_maria')
-    load_universidad_villa_maria = DummyOperator(task_id='load_universidad_villa_maria')
+    sql_universidad_villa_maria = PythonOperator(task_id='Query_villa_maria',
+                                                 python_callable=execute_queries,
+                                                 op_args=["villa_maria"],
+                                                 dag=dag)
 
     # Tasks order
-    sql_universidad_flores >> processing_universidad_flores >> load_universidad_flores
-
-    sql_universidad_villa_maria >> processing_universidad_villa_maria >> load_universidad_villa_maria
+    [sql_universidad_flores, sql_universidad_villa_maria]
